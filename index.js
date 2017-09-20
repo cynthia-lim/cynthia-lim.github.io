6c787c08e0d487be247b8520648964d9633dc7e8
cynthia-lim <32047334+cynthia-lim@users.noreply.github.com>
2017-09-20 16:33:26 +0800

    Finalized for the day

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Welcome to JS class")
+
+var student="frank"
+
+var student_1 = "Cynthia"
+
+var student_2 = "Christel"
+
+var welcome_msg = "Hi, let's be friends! " + student_1 + " , " + student_2
+console.log(welcome_msg)
